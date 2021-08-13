c6772f778f9cfa706f1a3365f3b6916cfaee8f9d
Patrick Jayoma <PatrickJayy@gmail.com>
2021-08-12 23:26:14 -0700

    Created structure of application

--- bball-stats-app.py
+++ bball-stats-app.py
@@ -6,3 +6,21 @@ import seaborn as sns
 import numpy as np
 
 st.title('NBA Player Stats:')
+
+st.markdown("""
+Performs simple webscrapping of NBA player stats and displays data!
+* **Python libraries:** base64, pandas, streamlmit
+* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
+""")
+
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2022))))
+
+
+# Web scrapping NBA player stats
+
+# Sidebar - Team selection 
+# Sidebar - Position selection
+# Data filtering
+# DL NBA player stats 
+# Heatmap    
